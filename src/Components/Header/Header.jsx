241367e128fe52161e251d645f13e382d88b271e
KausarAhmadTasin <kausarahmadtasinkat@gmail.com>
2023-12-30 18:13:30 +0600

    initiated and routes created

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,15 @@
+const Header = () => {
+  return (
+    <>
+      <ul>
+        <li>Home</li>
+        <li>Jobs</li>
+        <li>About</li>
+        <li>Contact</li>
+        <li>Signup</li>
+      </ul>
+    </>
+  );
+};
+
+export default Header;
---
+++ src/Routes/routes.jsx
@@ -0,0 +1,41 @@
+import { createBrowserRouter } from "react-router-dom";
+import App from "../App";
+import Home from "../Pages/Home/Home";
+import About from "../Pages/About/About";
+import Contact from "../Pages/Contact/Contact";
+import Jobs from "../Pages/Jobs/Jobs";
+import Signup from "../Pages/Signup/Signup";
+import NotFound from "../Components/Notfound/NotFound";
+
+const routes = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+      {
+        path: "/jobs",
+        element: <Jobs />,
+      },
+      {
+        path: "/signup",
+        element: <Signup />,
+      },
+    ],
+  },
+  {
+    path: "*",
+    element: <NotFound />,
+  },
+]);
